82e99a975583a974e8e9924db26fca572ef44375
anibal1409 <anibal.antonio1409@gmail.com>
2024-07-20 02:43:54 -0400

    feat: integrar imagen de login

---
+++ dist/dashboard-sdk/esm2022/lib/api/index.mjs
@@ -0,0 +1,11 @@
+export * from './api';
+export * from './auth.service';
+export * from './categories.service';
+export * from './customers.service';
+export * from './default.service';
+export * from './orders.service';
+export * from './products.service';
+export * from './sale.service';
+export * from './statistics.service';
+export * from './users.service';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi9wcm9qZWN0cy9kYXNoYm9hcmQtc2RrL3NyYy9saWIvYXBpL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLGNBQWMsT0FBTyxDQUFDO0FBQ3RCLGNBQWMsZ0JBQWdCLENBQUM7QUFDL0IsY0FBYyxzQkFBc0IsQ0FBQztBQUNyQyxjQUFjLHFCQUFxQixDQUFDO0FBQ3BDLGNBQWMsbUJBQW1CLENBQUM7QUFDbEMsY0FBYyxrQkFBa0IsQ0FBQztBQUNqQyxjQUFjLG9CQUFvQixDQUFDO0FBQ25DLGNBQWMsZ0JBQWdCLENBQUM7QUFDL0IsY0FBYyxzQkFBc0IsQ0FBQztBQUNyQyxjQUFjLGlCQUFpQixDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnLi9hcGknO1xuZXhwb3J0ICogZnJvbSAnLi9hdXRoLnNlcnZpY2UnO1xuZXhwb3J0ICogZnJvbSAnLi9jYXRlZ29yaWVzLnNlcnZpY2UnO1xuZXhwb3J0ICogZnJvbSAnLi9jdXN0b21lcnMuc2VydmljZSc7XG5leHBvcnQgKiBmcm9tICcuL2RlZmF1bHQuc2VydmljZSc7XG5leHBvcnQgKiBmcm9tICcuL29yZGVycy5zZXJ2aWNlJztcbmV4cG9ydCAqIGZyb20gJy4vcHJvZHVjdHMuc2VydmljZSc7XG5leHBvcnQgKiBmcm9tICcuL3NhbGUuc2VydmljZSc7XG5leHBvcnQgKiBmcm9tICcuL3N0YXRpc3RpY3Muc2VydmljZSc7XG5leHBvcnQgKiBmcm9tICcuL3VzZXJzLnNlcnZpY2UnO1xuIl19
